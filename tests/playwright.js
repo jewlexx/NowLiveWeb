28a9787ade22c02610babaa9f6e2f0cc1fb2980c
jamesinaxx <grassojames5@gmail.com>
2021-07-13 03:53:21 +0000

    Set the script to run all browsers

--- tests/playwright.js
+++ tests/playwright.js
@@ -3,8 +3,8 @@ const pw = require("playwright");
 const browsers = ["firefox", "webkit", "chromium"];
 
 (async () => {
-  browsers.forEach(async (browser) => {
-    const browser = await pw["browser"].launch();
+  browsers.forEach(async (bw) => {
+    const browser = await pw[bw].launch();
     const page = await browser.newPage();
     await page.goto("http://whatsmyuseragent.org/");
     await page.screenshot({ path: `example.png` });
