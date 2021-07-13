baa98fed57254525450c89c0352d45d468a09dbe
jamesinaxx <grassojames5@gmail.com>
2021-07-13 03:53:46 +0000

    Removed unnesacery async function

--- tests/playwright.js
+++ tests/playwright.js
@@ -2,12 +2,10 @@ const pw = require("playwright");
 
 const browsers = ["firefox", "webkit", "chromium"];
 
-(async () => {
-  browsers.forEach(async (bw) => {
-    const browser = await pw[bw].launch();
-    const page = await browser.newPage();
-    await page.goto("http://whatsmyuseragent.org/");
-    await page.screenshot({ path: `example.png` });
-    await browser.close();
-  });
-})();
+browsers.forEach(async (bw) => {
+  const browser = await pw[bw].launch();
+  const page = await browser.newPage();
+  await page.goto("http://whatsmyuseragent.org/");
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
+});
