5ff80b87aa8e74eddb5a6ff9be3eea032a7893c9
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-20 11:36:33 +1000

    Updated titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import Head from 'next/head';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Now Live</title>
+				<meta
+					name='description'
+					content='Website for the Now Live chrome extension'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 export default App;
