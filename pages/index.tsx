c1c6d797de84874ca2a66e92d65a49ef4d782352
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-03 08:03:10 +1000

    Use next/link for auth href

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,38 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Coming Soon!</title>
-				<meta
-					name='description'
-					content='Website for the Now Live chrome extension'
-				/>
-			</Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Coming Soon!</title>
+        <meta
+          name='description'
+          content='Website for the Now Live chrome extension'
+        />
+      </Head>
 
-			<main className={styles.main}>
-				<h1>Coming Soon!</h1>
-				<p id='NowLiveExtensionInstalledOrNotText'>
-					In the meantime, you can install the extension from{' '}
-					<a>here (not yet released)</a>
-				</p>
-			</main>
+      <main className={styles.main}>
+        <h1>Coming Soon!</h1>
+        <p id='NowLiveExtensionInstalledOrNotText'>
+          In the meantime, you can install the extension from{' '}
+          <a>here (not yet released)</a>
+        </p>
+        <p>
+          If you have installed NowLive, you might be looking for the{' '}
+          <Link href='/auth'>Auth Page</Link>
+        </p>
+      </main>
 
-			<footer className={styles.footer}>
-				<p>
-					Developed by{' '}
-					<a
-						href='https://github.com/jamesinaxx'
-						className={styles.userLink}
-					>
-						jamesinaxx
-					</a>
-				</p>
-			</footer>
-		</div>
-	);
+      <footer className={styles.footer}>
+        <p>
+          Developed by{' '}
+          <a href='https://github.com/jamesinaxx' className={styles.userLink}>
+            jamesinaxx
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
