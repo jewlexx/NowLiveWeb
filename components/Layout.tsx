1a2b62de4c337fc07af1a24a9ceb2f5d529afe3a
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-03 11:33:13 +1000

    Updated titles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import _ from 'lodash';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
+  function getPathName(): string {
+    const path = _.startCase(router.pathname.split('-').join(' ')).substr(1);
+
+    return path + ' - Now Live';
+  }
+
   return (
     <div>
       <Head>
-        <title>
-          {router.pathname === '/'
-            ? 'Now Live'
-            : router.pathname + ' - Now Live'}
-        </title>
+        <title>{router.pathname === '/' ? 'Now Live' : getPathName()}</title>
       </Head>
       {children}
     </div>
