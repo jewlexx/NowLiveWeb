cdfe66e74ce319610dcc6e2cfd7bbd43d0500c29
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-27 21:43:11 +1000

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../styles/Home.module.scss';
 import Page from '../components/Page';
+import styles from '../styles/Home.module.scss';
 
 export default function Home(): JSX.Element {
   return (
-    <Page
-      // className={styles.container}
-      description='Website for the Now Live chrome extension'
-    >
+    <Page description='Website for the Now Live chrome extension'>
       <main className={styles.main}>
         <h1>Coming Soon!</h1>
         {/* This is so, if the extension is installed, we can change the text */}
