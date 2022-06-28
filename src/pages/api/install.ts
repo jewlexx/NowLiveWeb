264f2ddc268ff6b3b860d646c8ad20496e2d30a5
Juliette Cordor <grassojames5@gmail.com>
2022-06-28 16:51:22 +1000

    added user agent parsing

--- src/pages/api/install.ts
+++ src/pages/api/install.ts
@@ -1,10 +1,31 @@
 import { NextApiHandler } from 'next';
+import { UAParser } from 'ua-parser-js';
 
-const installUrl =
-  'https://addons.mozilla.org/firefox/downloads/file/3968448/2dbb80bb3bfd4d999aad-1.1.1.xpi';
+const handler: NextApiHandler = (req, res) => {
+  const userAgentString = req.headers['user-agent'];
 
-const handler: NextApiHandler = (_, res) => {
-  res.status(302).redirect(installUrl);
+  if (userAgentString === undefined) {
+    res.status(400).json({ error: 'User agent is required' });
+    return;
+  }
+
+  const userAgent = UAParser(userAgentString);
+
+  if (userAgent.browser.name === 'Firefox') {
+    res
+      .status(302)
+      .redirect(
+        'https://addons.mozilla.org/firefox/downloads/file/3968448/2dbb80bb3bfd4d999aad-1.1.1.xpi',
+      );
+  } else if (userAgent.browser.name === 'Chrome') {
+    res
+      .status(302)
+      .redirect(
+        'https://chrome.google.com/webstore/detail/now-live/fonhghodpbmhkkccljcjkpjjooehflpk',
+      );
+  } else {
+    res.status(400).json({ error: 'Unsupported browser' });
+  }
 };
 
 export default handler;
