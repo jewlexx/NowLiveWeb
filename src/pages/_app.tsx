0cf734aebee93f49d824702bd1e621dadc851c88
Juliette Cordor <professional@maybejules.com>
2024-06-19 23:35:55 +1000

    remove pages folder

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.scss';
-
-function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
-export default App;
