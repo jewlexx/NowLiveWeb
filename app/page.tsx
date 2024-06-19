4a0db7ad9eef75d0d7a3383c6205c56783c9ce80
Juliette Cordor <professional@maybejules.com>
2024-06-20 00:06:48 +1000

    added download button animations

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import { type Metadata } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './home.module.scss';
+import {
+  IconBrandChrome,
+  IconBrandFirefox,
+  IconDownload,
+} from '@tabler/icons-react';
 
 export const metadata: Metadata = {
   title: 'Now Live',
@@ -13,30 +18,34 @@ export default function Home(): JSX.Element {
     <>
       <span>
         <Link
-          className={styles.link}
+          className={styles.linkbutton}
           href='/install?browser=Firefox'
           target='_blank'
           rel='noreferrer'
         >
-          <Image
+          {/* <Image
             src='https://user-images.githubusercontent.com/585534/107280546-7b9b2a00-6a26-11eb-8f9f-f95932f4bfec.png'
             alt='install for firefox'
             width={183}
             height={64}
-          />
+          /> */}
+          <IconBrandFirefox /> <span>Install for Firefox</span>
+          <IconDownload className={styles.downloadIcon} />
         </Link>
         <Link
-          className={styles.link}
+          className={styles.linkbutton}
           href='/install?browser=Chrome'
           target='_blank'
           rel='noreferrer'
         >
-          <Image
+          {/* <Image
             src='https://user-images.githubusercontent.com/585534/107280622-91a8ea80-6a26-11eb-8d07-77c548b28665.png'
             alt='install on the chrome web store'
             width={228}
             height={64}
-          />
+          /> */}
+          <IconBrandChrome /> <span>Install for Google Chrome</span>
+          <IconDownload className={styles.downloadIcon} />
         </Link>
       </span>
       <p>
